5601fbb65536199276026984df70f88e12b2a53a
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-03-14 20:03:00 +0530

    Updated actions.

--- frontend/controllers/SubscriptionController.php
+++ frontend/controllers/SubscriptionController.php
@@ -2,9 +2,12 @@
 namespace cmsgears\subscription\frontend\controllers;
 
 // Yii Imports
-use \Yii; 
+use \Yii;
+use yii\filters\VerbFilter;
+use yii\web\NotFoundHttpException;
 
 // CMG Imports
+use cmsgears\core\common\config\CoreGlobal;
 use cmsgears\core\frontend\config\WebGlobalCore;
 
 class SubscriptionController extends \cmsgears\core\frontend\controllers\base\Controller {
@@ -19,14 +22,37 @@ class SubscriptionController extends \cmsgears\core\frontend\controllers\base\Co
 	// Instance Methods --------------------------------------------
 
 	// yii\base\Component ----------------
- 
-	
-	// FeatureController -----------------
 
-	public function actionIndex() {
-		 
-		$this->layout	= WebGlobalCore::LAYOUT_PUBLIC; 
-		 
+    public function behaviors() {
+
+        return [
+            'rbac' => [
+                'class' => Yii::$app->cmgCore->getRbacFilterClass(),
+                'actions' => [
+	                'all'  => [ 'permission' => CoreGlobal::PERM_USER ]
+                ]
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+	                'all'  => [ 'get' ],
+	                'plans'  => [ 'get' ]
+                ]
+            ]
+        ];
+    }
+
+	// SubscriptionController ------------
+
+	public function actionAll() {
+
+	    return $this->render( 'all' );
+	}
+
+	public function actionPlans() {
+
+		$this->layout	= WebGlobalCore::LAYOUT_PUBLIC;
+
 	    return $this->render( 'index' );
 	}
 }
